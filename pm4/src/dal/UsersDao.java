fe6c72b079cb62d8ac4ef8a61afd862be8e7f103
wenhua <yao.wen@husky.neu.edu>
2019-11-11 22:34:29 -0800

    added other daos

---
+++ pm4/src/dal/UsersDao.java
@@ -0,0 +1,146 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.Persons;
+import model.Users;
+
+public class UsersDao extends PersonsDao {
+
+  private static UsersDao instance = null;
+
+  protected UsersDao() {
+    super();
+  }
+
+  public static UsersDao getInstance() {
+    if (instance == null) {
+      instance = new UsersDao();
+    }
+    return instance;
+  }
+
+  public Users create(Users user) throws SQLException {
+    Persons person = create(new Persons(user.getUserId(), user.getUserName(), user.getPassword()));
+
+    String insertUser = "INSERT INTO Users(UserId,FirstName,LastName,Email,Phone,Street,City,State,Zip,Level) VALUES(?,?,?,?,?,?,?,?,?,?);";
+    Connection connection = null;
+    PreparedStatement insertStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      insertStmt = connection.prepareStatement(insertUser);
+      insertStmt.setInt(1, person.getUserId());
+      insertStmt.setString(2, user.getFirstName());
+      insertStmt.setString(3, user.getLastName());
+      insertStmt.setString(4, user.getEmail());
+      insertStmt.setString(5, user.getPhone());
+      insertStmt.setString(6, user.getStreet());
+      insertStmt.setString(7, user.getCity());
+      insertStmt.setString(8, user.getState());
+      insertStmt.setString(9, user.getZip());
+      insertStmt.setString(10, user.getUserLevel().name());
+
+      insertStmt.executeUpdate();
+      user.setUserId(person.getUserId());
+      return user;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if (connection != null) {
+        connection.close();
+      }
+      if (insertStmt != null) {
+        insertStmt.close();
+      }
+    }
+  }
+
+  public Users getUserById(int userId) throws SQLException {
+    String selectUsers =
+        "SELECT Users.UserId AS UserId, UserName, Password, FirstName, LastName, Email, Phone, Street"
+            +
+            "City, State, Zip, Level" +
+            "FROM Users INNER JOIN Persons " +
+            "  ON Users.UserId = Persons.UserId " +
+            "WHERE Users.UserId=?;";
+    Connection connection = null;
+    PreparedStatement selectStmt = null;
+    ResultSet results = null;
+    try {
+      connection = connectionManager.getConnection();
+      selectStmt = connection.prepareStatement(selectUsers);
+      selectStmt.setInt(1, userId);
+      results = selectStmt.executeQuery();
+
+      if (results.next()) {
+
+        String userName = results.getString("UserName");
+        String password = results.getString("Password");
+        String firstName = results.getString("FirstName");
+        String lastName = results.getString("LastName");
+        String email = results.getString("Email");
+        String phone = results.getString("Phone");
+        String street = results.getString("Street");
+        String city = results.getString("City");
+        String state = results.getString("State");
+        String zip = results.getString("Zip");
+//				Restaurants.CuisineType cuisineType = Restaurants.
+//						CuisineType.valueOf(results.getString("CuisineType"));
+        Users.UserLevel level = Users.UserLevel.valueOf(results.getString("Level"));
+
+        Users user = new Users(userId, userName, password, firstName, lastName, email, phone,
+            street, city, state, zip, level);
+        return user;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if (connection != null) {
+        connection.close();
+      }
+      if (selectStmt != null) {
+        selectStmt.close();
+      }
+      if (results != null) {
+        results.close();
+      }
+    }
+    return null;
+  }
+
+
+  public Users delete(Users user) throws SQLException {
+    String deleteUser = "DELETE FROM Users WHERE UserId=?;";
+
+    Connection connection = null;
+    PreparedStatement deleteStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      deleteStmt = connection.prepareStatement(deleteUser);
+      deleteStmt.setInt(1, user.getUserId());
+      int affectedRows = deleteStmt.executeUpdate();
+      if (affectedRows == 0) {
+        throw new SQLException("No records available to delete for UserName=" + user.getUserId());
+      }
+
+      super.delete(user);
+
+      return null;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if (connection != null) {
+        connection.close();
+      }
+      if (deleteStmt != null) {
+        deleteStmt.close();
+      }
+    }
+
+  }
+}
