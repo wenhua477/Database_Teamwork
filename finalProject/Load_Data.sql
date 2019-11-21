81ffeaac51fe8b4c807818235afebd9442201434
JWYDSN <40532497+startoday@users.noreply.github.com>
2019-11-21 00:34:48 -0800

    Update Load_Data.sql

--- finalProject/Load_Data.sql
+++ finalProject/Load_Data.sql
@@ -1,20 +1,20 @@
 USE PM2;
 
-LOAD DATA LOCAL INFILE '/Users/zoeyw/Documents/database5200/PMs/PM4/Database_Teamwork/Database_Teamwork/finalProject/Campsites_withFipId.csv' INTO TABLE StarGazingPlaces
+LOAD DATA LOCAL INFILE '/Users/zoeyw/Documents/database5200/PMs/fp/Database_Teamwork/finalProject/Campsites_new.csv' INTO TABLE StarGazingPlaces
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (@col1,@col2,@dummy, @col4) set Latitude=@col1,Longitude=@col2,State=@col4;
   
   
-LOAD DATA LOCAL INFILE '/Users/zoeyw/Documents/database5200/PMs/PM4/Database_Teamwork/Database_Teamwork/finalProject/Campsites_withFipId.csv' INTO TABLE Campsites
+LOAD DATA LOCAL INFILE '/Users/zoeyw/Documents/database5200/PMs/fp/Database_Teamwork/finalProject/Campsites_new.csv' INTO TABLE Campsites
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
-  (@col1, @col2, @col3,@dummy,@col5,@col6,@dummy,@dummy,@col9) set Name=@col3,Type=@col6,Capacity=@col5,fips=@col9;
+  (@col1, @col2, @col3,@dummy,@col5,@col6,@dummy,@col8) set Name=@col3,Type=@col6,Capacity=@col5,fips=@col8;
   
   
-LOAD DATA LOCAL INFILE '/Users/zoeyw/Documents/database5200/PMs/PM4/Database_Teamwork/Database_Teamwork/finalProject/populationAndElevation.csv' INTO TABLE LocationInfo
+LOAD DATA LOCAL INFILE '/Users/zoeyw/Documents/database5200/PMs/fp/Database_Teamwork/finalProject/populationAndElevation.csv' INTO TABLE LocationInfo
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
@@ -22,7 +22,7 @@ LOAD DATA LOCAL INFILE '/Users/zoeyw/Documents/database5200/PMs/PM4/Database_Tea
   set Latitude=@col1,Longitude=@col2,Elevation = @col3,Population =@col4,
     State= @col5,County= @col6,fips=@col7;
     
-LOAD DATA LOCAL INFILE '/Users/zoeyw/Documents/database5200/PMs/PM4/Database_Teamwork/Database_Teamwork/finalProject/crimeMap.csv' INTO TABLE CountyInfo
+LOAD DATA LOCAL INFILE '/Users/zoeyw/Documents/database5200/PMs/fp/Database_Teamwork/finalProject/crimeMap.csv' INTO TABLE CountyInfo
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
